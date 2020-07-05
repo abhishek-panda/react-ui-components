8f0fe44632f5a38bbdb6f11b189f28a2eea09494
Abhishek Panda <panda.abhis@gmail.com>
2020-07-05 10:29:48 +0530

    Tesf

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,32 +2,32 @@ import * as React from 'react';
 import bind from '../utils/bind';
 
 interface Props {
-    children : React.ReactChild,
-    msg ?: string
+  children : React.ReactChild,
+  msg ?: string
 }
 
-
 interface State {
-    count : number
+  count : number
 }
 
 export class ButtonComponent extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            count: 0,
-        };
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
 
-    @bind
-    clickHandler () {
-        console.log(this.state);
-    }
+  @bind
+  private clickHandler(): number {
+    const { count } = this.state;
+    return count;
+  }
 
-    render() {
-        return <button onClick={this.clickHandler}>{this.props.children}</button>
-    }
+  render() {
+    const { children } = this.props;
+    return <button type="button" onClick={this.clickHandler}>{children}</button>;
+  }
 }
-
 
 export default ButtonComponent;
